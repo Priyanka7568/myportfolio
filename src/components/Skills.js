540e753895dd9aa8571299407c8254a486240a72
Priyanka Meena <pittu31meena@gmail.com>
2022-06-14 00:13:57 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,9 +2,9 @@ import React from "react";
 import SkillsList from "../assets/JSON/skillsList";
 function Skills() {
   return (
-    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300 sm_padding_top">
+    <div name="skills" className="w-full bg-[#0a192f] text-gray-300 sm_padding_top">
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto px-4 flex flex-col justify-center w-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600">
             Skills

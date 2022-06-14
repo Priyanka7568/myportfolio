1898b4d6f67f8f2d128271b49a857414eab6afee
Priyanka Meena <pittu31meena@gmail.com>
2022-06-14 19:37:44 +0530

    Update Work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -20,19 +20,18 @@ function Work() {
                   style={{ backgroundImage: `url(${image})` }}
                   className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
                 >
-                  {/* Hover Effects */}
                   <div className="opacity-0 group-hover:opacity-100 text-center">
                     <span className="text-2xl font-bold text-white tracking-wider">
                       {title}
                     </span>
                     <p>{technology}</p>
                     <div className="pt-8 text-center">
-                      <a href={demo_URL} target="./blank">
+                      <a href={demo_URL} target="_blank">
                         <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                           Demo
                         </button>
                       </a>
-                      <a href={code_URL} target="./blank">
+                      <a href={code_URL} target="_blank">
                         <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                           Code
                         </button>
